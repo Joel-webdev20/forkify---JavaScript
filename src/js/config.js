02b1fefb759b7d1629f956cde2d944d0bf33d27f
Joel <128468899+Joel-webdev20@users.noreply.github.com>
2024-05-08 18:15:39 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6c656685-20d9-4e02-9f6a-e08581e0576a';
+export const MODAL_CLOSE_SEC = 2.5;
